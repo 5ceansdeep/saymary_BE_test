48e8e4d9b72907064e3adc333001e09ce6874616
5ceansdeep <su5653@naver.com>
2025-07-28 06:14:43 +0900

    @PostMapping("/reset-password") 수정

---
+++ src/main/java/com/example/user/dto/PasswordResetRequest.java
@@ -0,0 +1,16 @@
+package com.example.user.dto;
+
+
+public class PasswordResetRequest {
+    private String email;
+
+    public PasswordResetRequest() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
